27a53c37be69e5ed278c943cf0347bb3a824d6b9
ivgtr <ivgtr.me@gmail.com>
2023-12-30 04:48:21 +0900

    fix

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -1,6 +1,7 @@
-import { Images } from "@/types/api";
+import type { Image } from "@/types/api";
+import { motion } from "framer-motion";
 
-export const ImageViewer = ({ image, zIndex }: { image: Images[0]; zIndex: number }) => {
+export const ImageViewer = ({ image, zIndex }: { image: Image; zIndex: number }) => {
   return (
     <div
       className="absolute top-0 left-0 w-full h-full bg-slate-400 grid grid-cols-1"
@@ -8,10 +9,14 @@ export const ImageViewer = ({ image, zIndex }: { image: Images[0]; zIndex: numbe
         zIndex,
       }}
     >
-      <div className="absolute w-full h-full block">
-        <img src={image.fullsize} alt={image.alt} className="absolute inset-0 object-contain w-full h-full z-20" />
-        <div className="absolute inset-0 object-cover w-full h-full bg-black opacity-30 z-10"></div>
-        <img src={image.fullsize} alt={image.alt} className="absolute inset-0 object-cover w-full h-full blur-sm" />
+      <div className="absolute w-full h-full block overflow-hidden">
+        <div className="absolute inset-0 w-full h-full z-30">
+          <img src={image.fullsize} alt={image.alt} className="object-contain w-full h-full z-20" />
+        </div>
+        <div className="w-full h-full bg-black opacity-30 z-10"></div>
+        <div className="absolute inset-0 w-full h-full">
+          <img src={image.fullsize} alt={image.alt} className="object-cover w-full h-full blur-sm" />
+        </div>
       </div>
     </div>
   );
