da34d4ac81edf04c6bd225bfe83dae282c6e91c0
ivgtr <ivgtr.me@gmail.com>
2023-12-30 08:06:30 +0900

    Add @vercel/analytics package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { DefaultLayout } from "@/layout/DefaultLayout";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <DefaultLayout>
       <Component {...pageProps} />
+      <Analytics />
     </DefaultLayout>
   );
 }
