48a3a280dd1f36fd6a5a32aab9654344bfbc7ace
ivgtr <ivgtr.me@gmail.com>
2023-12-30 06:12:00 +0900

    add

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,21 +1,27 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Oswald } from "next/font/google";
+import { GithubPicker } from "react-color";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
 export const Clock = () => {
   const timerRef = useRef<ReturnType<typeof setInterval>>();
   const [time, setTime] = useState<Date | null>(null);
+  const [isShowColorPicker, setIsShowColorPicker] = useState<boolean>(false);
+  const [color, setColor] = useState<string>("#93c5fd");
+  const [isReady, setIsReady] = useState<boolean>(false);
   const timeString = useMemo(() => {
     if (time === null) return "";
     // HH:mm 形式で表示
     return `${time.getHours().toString().padStart(2, "0")}:${time.getMinutes().toString().padStart(2, "0")}`;
   }, [time]);
 
+  const handleColorChange = useCallback((color: { hex: string }) => {
+    setColor(color.hex);
+    setIsShowColorPicker(false);
+  }, []);
+
   useEffect(() => {
-    if (time === null) {
-      setTime(new Date());
-    }
     if (timerRef.current !== undefined) {
       clearInterval(timerRef.current);
     }
@@ -30,9 +36,70 @@ export const Clock = () => {
     };
   }, []);
 
+  // localStorage に保存
+  useEffect(() => {
+    if (!isReady) return;
+    localStorage.setItem("color", color);
+  }, [color, isReady]);
+
+  // localStorage から読み込み
+  useEffect(() => {
+    const color = localStorage.getItem("color");
+    if (color !== null) {
+      setColor(color);
+    }
+    setIsReady(true);
+    setTime(new Date());
+  }, []);
+
   return (
     <div className="absolute flex justify-start left-0 top-0 z-10 w-full">
-      {time !== null && <div className={`text-9xl text-blue-300 font-bold ${oswald.className} m-4`}>{timeString}</div>}
+      {time !== null && isReady && (
+        <>
+          <div
+            className="m-4 cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              setIsShowColorPicker(true);
+            }}
+          >
+            <p
+              className={`text-9xl font-bold ${oswald.className}`}
+              style={{
+                color,
+              }}
+            >
+              {timeString}
+            </p>
+            {isShowColorPicker && (
+              <GithubPicker
+                colors={[
+                  "#93c5fd",
+                  "#ffbdaf",
+                  "#f59898",
+                  "#ffce8a",
+                  "#f7ea7e",
+                  "#abe0c3",
+                  "#b08ef0",
+                  "#7ea3f7",
+                  "#a7e4fc",
+                  "#c1f5f5",
+                ]}
+                onChangeComplete={handleColorChange}
+                styles={{
+                  default: {
+                    card: {
+                      marginTop: "0.5rem",
+                      marginLeft: "1rem",
+                    },
+                  },
+                }}
+              />
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
