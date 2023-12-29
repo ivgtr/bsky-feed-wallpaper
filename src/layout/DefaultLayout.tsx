e56f16edd0c6bc24da2c2444017bc3a0cd5d3080
ivgtr <ivgtr.me@gmail.com>
2023-12-30 04:01:22 +0900

    fix

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -4,9 +4,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main
-      className={`flex min-h-screen h-full flex-col items-center justify-between overflow-hidden ${inter.className}`}
-    >
+    <main className={`flex min-h-svh h-full flex-col items-center justify-between overflow-hidden ${inter.className}`}>
       {children}
     </main>
   );
